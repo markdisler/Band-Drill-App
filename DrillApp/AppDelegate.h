7c8e747d55b8d2046ebb82fe95190e657c31ac7e
Mark <mldisler@gmail.com>
2018-03-24 16:37:29 -0400

    Initial Commit

---
+++ DrillApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrillApp
+//
+//  Created by Mark Disler on 3/24/18.
+//  Copyright © 2018 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
